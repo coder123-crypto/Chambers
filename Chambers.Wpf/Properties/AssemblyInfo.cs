d0d8928fafcb1fb8b89ba2efd13dd81a73ff6838
lereminbiz@gmail.com <leremin>
2023-06-05 09:50:51 +0300

    Добавлено xmlns

---
+++ Chambers.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://polus.local/chambers/wpf", "chamberswpf")]
+[assembly: XmlnsDefinition("http://polus.local/chambers/wpf", "Chambers.Wpf")]
